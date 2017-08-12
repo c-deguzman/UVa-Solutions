ca1164d1f40b452cefa03f486f806260742c8287
Christopher DeGuzman <c.deguzman@email.com>
2017-08-12 10:41:31 -0400

    Add files via upload

---
+++ 12210 - A Match Making Problem.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, tc;
+int young;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    while (scanf(" %d %d", &n, &m) == 2){
+        if (n == 0 && m == 0){
+            break;
+        }
+
+        young = 61;
+
+        for (int i=0; i < n; i++){
+            int age;
+            scanf(" %d", &age);
+            young = min(young, age);
+        }
+
+        for (int i=0; i < m; i++){
+            int age;
+            scanf(" %d", &age);
+        }
+
+        printf("Case %d: ", ++tc);
+        if (n <= m){
+            printf("%d\n", 0);
+        } else {
+            printf("%d %d\n", n - m, young);
+        }
+    }
+}
