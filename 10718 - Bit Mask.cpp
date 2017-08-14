1996bd230e6a401bf093df5163a12efbdfefc2a0
Christopher DeGuzman <c.deguzman@email.com>
2017-08-14 15:36:10 -0400

    Add files via upload

--- 10718 - Bit Mask.cpp
+++ 10718 - Bit Mask.cpp
@@ -5,19 +5,15 @@ using namespace std;
 
 long long n, l, u;
 
-long long reconcile(long long num){
+long long solve(void){
 
-    if (l <= num && num <= u){
-        return num;
-    }
+    long long num = 0;
 
     for (int i=31; i >= 0; i--){
-        long long diff = l - num;
         long long bit = ((long long) 1 << i);
 
-        if (bit <= diff && (num | bit) <= u){
-            num |= bit;
-        } else if ( (num | (bit - 1)) < l){
+        if (((n & bit) && ((num | bit) <= l)) ||
+            (!(n & bit) && ((num | bit) <= u))){
             num |= bit;
         }
     }
@@ -25,36 +21,8 @@ long long reconcile(long long num){
     return num;
 }
 
-long long solve(void){
-
-    long long num = 0;
-
-    bool match = true;
-
-    for (int i=31; i >= 0; i--){
-        long long bit = ((long long) 1 << i);
-
-        if (match && (l & bit & u)){
-            num |= bit;
-        } else if (match && (l & bit) != (u & bit)){
-            match = false;
-        }
-
-        if (!match){
-            if (!(n & bit)){
-                if ((num | bit) <= u){
-                    num |= bit;
-                }
-            }
-        }
-    }
-
-    return reconcile(num);
-}
-
 int main()
 {
-
     ios::sync_with_stdio(false);
 
     while (scanf(" %lld %lld %lld", &n, &l, &u) == 3){
